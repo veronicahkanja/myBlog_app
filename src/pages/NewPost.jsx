723a2338ecea4f22db99a703e5623bad172f5167
Veronicah Kanja <kanjaveronicah@gmail.com>
2025-12-02 22:03:29 +0300

    POST request to JSON server

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -6,11 +6,19 @@ function NewPost() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
-
-
-
-
-  
+function handleSubmit(e) {
+    e.preventDefault(); 
+fetch("http://localhost:8000/posts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, body }),
+    })
+      .then(() => {
+        // After creating post, go back to Home page
+        navigate("/");
+      })
+      .catch((err) => console.error("Error creating post:", err));
+}
   return (
     <div>
       
@@ -18,4 +26,4 @@ function NewPost() {
   )
 }
 
-export default NewPost
+export default NewPost;
