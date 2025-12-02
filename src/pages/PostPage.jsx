6e5ad9ba70f36c7827e52e4ecc36032dea888691
Veronicah Kanja <kanjaveronicah@gmail.com>
2025-12-02 21:51:14 +0300

    Fetch post from Json Server

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -5,6 +5,14 @@ function PostPage() {
     const { id } = useParams(); // Get post ID from Url
     const navigate = useNavigate();
     const [post, setPosts] = useState(null); // Storee single post
+
+// Fetch post from JSON Server
+useEffect(() =>{
+     fetch(`http://localhost:8000/posts/${id}`)
+      .then((res) => res.json())
+      .then((data) => setPost(data))
+      .catch((err) => console.error("Error fetching post:", err));
+}, [id]);    
   return (
     <div>
       
