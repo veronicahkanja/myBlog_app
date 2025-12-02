c65c261262e2fc1ec6c27c8c3f836ed93404e25e
Veronicah Kanja <kanjaveronicah@gmail.com>
2025-12-02 20:46:08 +0300

    added router and routes to my app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PostPage from "./pages/PostPage";
+import NewPost from "./pages/NewPost";
+import EditPost from "./pages/EditPost";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts/:id" element={<PostPage />} />
+        <Route path="/new" element={<NewPost />} />
+        <Route path="/edit/:id" element={<EditPost />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
+export default App;
