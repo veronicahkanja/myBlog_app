a740c85cd925199c3f4ae4f8d3d34705ed8766b4
Veronicah Kanja <kanjaveronicah@gmail.com>
2025-12-02 20:54:16 +0300

    fetch posts when componeent loads

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,15 @@ import { Link } from "react-router-dom";
 
 function Home() {
     const [posts, setPosts] = useState([]); // here we have used State to store posts
+
+    // Fetch posts when component Loads
+
+    useEffect(() => {
+        fetch("http://localhost:8000/posts") // this is our JSON server endpoint
+        .then ((res) => res.json())
+        .then ((data) => setPosts(data)) // save posts in store
+        .catch((err) => console.error("Error fetching posts:", err));
+    }, []);
   return (
     <div>
       
