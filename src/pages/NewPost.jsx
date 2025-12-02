36a9f2655bb894aa9d8ee0969407f8cd4d756d48
Veronicah Kanja <kanjaveronicah@gmail.com>
2025-12-02 22:01:55 +0300

    Function to handle form submission

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function NewPost() {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+
+
+
+
+  
   return (
     <div>
       
