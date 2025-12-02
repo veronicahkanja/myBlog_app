577ceb6678449c3319feae1d03d7fb495771f952
Veronicah Kanja <kanjaveronicah@gmail.com>
2025-12-02 20:49:36 +0300

    we have used State to store posts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+
+
 
 function Home() {
+    const [posts, setPosts] = useState([]); // here we have used State to store posts
   return (
     <div>
       
