6fdb10799aedca8d263bed89c908ad8d858f0218
Veronicah Kanja <kanjaveronicah@gmail.com>
2025-12-03 10:09:27 +0300

    Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 function Header() {
+   const headerStyle = {
+    padding: "15px 30px",
+    backgroundColor: "#222",
+    color: "white",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  };
   return (
     <div>
       
